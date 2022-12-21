104a5f89808c8d468d6e474a4fecd7066a00da74
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2022-12-21 13:33:12 +0530

    First UI
    added first name, last name, phone number and email labels. user can type their basic details and submit their information, then they will be able to continue their journey

---
+++ index.js
@@ -0,0 +1,5 @@
+let onButtonClick = function(){
+    document.getElementById("button").style.backgroundColor = "yellow";
+    button.innerHTML = "SUBMITED";
+    alert("Successfully Submited. Thank You.");
+}
